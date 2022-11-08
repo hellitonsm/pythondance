0249a128aae973864833dbaf2f70613b3caebe18
hellitonsm <hellitonsm@gmail.com>
2022-11-08 14:45:18 -0400

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,5 +1,18 @@
 from turtle import *
-#draw a Skycrapper
+
+#draw a Turtlestar
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
+
+#draw a Skycraper
 x=input()
 for i in range(0,x):
     for i in range(0,x-i-1):
